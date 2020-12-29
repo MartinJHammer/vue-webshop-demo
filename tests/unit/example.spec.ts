c897060def6e4df22b9560cbab2df38013e879e8
Martin <martin_hammer@live.dk>
2020-12-29 15:19:22 +0100

    Fixed test.

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import hello-world from '@/components/hello-world.vue';
+import HelloWorld from '@/components/hello-world/hello-world.vue';
 
 describe('hello-world.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(hello-world, {
+    const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
     expect(wrapper.text()).to.include(msg);
