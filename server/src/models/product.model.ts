1570b856fe86f071e8a9df2fda5259d3545d8a54
Martin <martin_hammer@live.dk>
2020-12-30 18:01:59 +0100

    Added hello controller and product model.

---
+++ server/src/models/product.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
