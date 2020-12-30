3661e6b8d605d6456ca7a30ef5d567098d9224bf
Martin <martin_hammer@live.dk>
2020-12-30 22:35:31 +0100

    Removed eslint

--- client/src/components/hello-world/hello-world.ts
+++ client/src/components/hello-world/hello-world.ts
@@ -1,7 +1,6 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { TestService } from '../../services/test.service';
 
-
 @Component<HelloWorld>({
   subscriptions() {
     return ({
--- client/src/services/test.service.ts
+++ client/src/services/test.service.ts
@@ -1,21 +1,25 @@
-import { interval } from 'rxjs';
+import { interval, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export class TestService {
     private static instance: TestService;
 
     private constructor() { }
-    public static getInstance(): TestService {
-        if (!TestService.instance) {
-            TestService.instance = new TestService();
-        }
 
-        return TestService.instance;
+    public static getInstance(): TestService {
+      if (!TestService.instance) {
+        TestService.instance = new TestService();
+      }
+
+      return TestService.instance;
     }
 
-    public getMessage$() {
-        return interval(1000).pipe(
-            map((count) => `We are now at: ${count}`),
-        );
+    /**
+     * test
+     */
+    public getMessage$(): Observable<string> {
+      return interval(1000).pipe(
+        map((count) => `We are now at: ${count}`),
+      );
     }
 }
